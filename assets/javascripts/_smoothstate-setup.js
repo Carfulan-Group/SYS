6b6a8fa141a97800c4f8ed9ef4980e6d0048e0cb
Sam Mckay <sam.mckay@aol.co.uk>
2016-04-19 14:12:48 +0100

    Made SPA using jQuery SmoothState, genreal styling improvements

---
+++ assets/javascripts/_smoothstate-setup.js
@@ -0,0 +1,57 @@
+// makes sure js is called on DOM ready
+function pageLoad ( $ )
+{
+	slider ( $ );
+	menu ( $ );
+	masonry ( $ );
+	machine ( $ );
+	home ( $ );
+	footer ( $ );
+	contactForm ( $ );
+	aside ( $ );
+
+	// set up for lazy loading images
+	new Layzr ( {
+		threshold : 70
+	} );
+}
+
+jQuery ( document ).ready ( function ( $ )
+{
+	//call pageLoad when the DOM is ready
+	pageLoad ( $ );
+	// makes sure the opacity is good n' proper
+	$ ( '.fade' ).addClass ( 'fade--in' );
+
+	// // // // // // // // // // // // // // //
+
+	// start smoothstate
+	$ ( '#smoothstate' ).smoothState ( {
+		// prefetches content before the user releases their finger (touchscreen)
+		prefetch : true,
+		allowFormCaching : false,
+		forms : '',
+
+		// runs once the ajax request starts
+		onStart : {
+			render : function ()
+			{
+				$ ( '.fade' ).removeClass ( 'fade--in' );
+				$ ( '.loading' ).addClass ( 'spin' );
+				$ ( 'body' ).animate ( { scrollTop : 0 }, '1000' );
+			}
+		},
+
+		// stuff to run once the new content has been loaded and added to the DOM
+
+		onAfter : function ()
+		{
+			pageLoad ( $ );
+			//$ ( window ).trigger ( 'load' );
+			$ ( '.loading' ).removeClass ( 'spin' );
+			$ ( '.fade' ).addClass ( 'fade--in' );
+		}
+
+	} ).data ( 'smoothState' )
+
+} ); // jquery DOM ready
