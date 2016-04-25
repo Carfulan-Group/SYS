fb0cde7af6e7352bb639338018a54c6a569c9bea
Sam Mckay <sam.mckay@aol.co.uk>
2016-04-25 09:45:50 +0100

    Neatened up the slider JS, fixed the problem where it would skip slides after being reloaded via smoothstate

--- assets/javascripts/_slider.js
+++ assets/javascripts/_slider.js
@@ -1,83 +1,105 @@
 function slider ()
 {
-
 	var element = document.querySelector ( '.slider' );
+
+	// // //
+	// the slider only runs this code if there is actually a slider in the DOM
+	// // //
 	if ( typeof(element) != 'undefined' && element != null )
 	{
+		document.querySelector ( ".slide:first-child" ).classList.add ( "first-slide" );
 
 		var slide        = document.querySelectorAll ( ".slide" ),
-			slidecounter = 0,
-			counter      = 1;
+			firstSlide   = document.querySelector ( '.first-slide' ),
+			slideCounter = 0,
+			counter      = 1,
+			oldMargin    = 0,
+			newMargin    = 0,
+			interval     = 5000,
+			theSlideShow;
 
-		Array.prototype.forEach.call ( slide, function ( el, i )
+		firstSlide.setAttribute ( 'data-left', '0' );
+
+		Array.prototype.forEach.call ( slide, function ()
 		{
-			slidecounter ++;
+			slideCounter ++;
 		} );
 
-		var sliderwidth = slidecounter * 100;
+		var sliderWidth = slideCounter * 100;
 
-		document.querySelector ( ".slider" ).style.width = sliderwidth + "%";
+		document.querySelector ( ".slider" ).style.width = sliderWidth + "%";
 
-		var slidewidth = 100 / slidecounter,
-			margin     = slidewidth;
+		var slideWidth = 100 / slideCounter,
+			margin     = slideWidth;
 
-		Array.prototype.forEach.call ( slide, function ( el, i )
+		Array.prototype.forEach.call ( slide, function ( el )
 		{
-			el.style.width = slidewidth + "%";
+			el.style.width = slideWidth + "%";
 		} );
 
-		document.querySelector ( ".slide:first-child" ).classList.add ( "first-slide" );
-		document.querySelector ( ".first-slide" ).setAttribute ( 'data-left', 0 );
-
-		function slideshow ()
+		// // //
+		// the function that actually moves the slides
+		// // //
+		function slideShow ()
 		{
-			if ( slidecounter > counter )
+			// // //
+			// if the slide isn't the last slide
+			// // //
+			if ( slideCounter > counter )
 			{
 				counter ++;
-				document.querySelector ( ".first-slide" ).style.marginLeft = "-" + margin + "%";
-				document.querySelector ( ".first-slide" ).setAttribute ( 'data-left', margin );
-				margin = margin + slidewidth;
+				firstSlide.style.marginLeft = "-" + margin + "%";
+				firstSlide.setAttribute ( 'data-left', margin );
+				margin = margin + slideWidth;
 			}
+			// // //
+			// if it is t he last slide
+			// // //
 			else
 			{
-				counter                                                    = 1;
-				document.querySelector ( ".first-slide" ).style.marginLeft = "0";
-				document.querySelector ( ".first-slide" ).setAttribute ( 'data-left', 0 );
-				margin = slidewidth;
+				counter                     = 1;
+				firstSlide.style.marginLeft = "0";
+				firstSlide.setAttribute ( 'data-left', 0 );
+				margin = slideWidth;
 			}
 		}
 
-		var startslideshow = setInterval ( function ()
+		// // //
+		// function that make the slides change every X (set in var interval) seconds
+		// // //
+		function startSlideShow ()
 		{
-			slideshow ()
-		}, 5000 );
+			theSlideShow = setInterval ( function ()
+			{
+				slideShow ();
+			}, interval );
+		}
 
 		document.getElementById ( "pauseSlider" ).onclick = function ()
 		{
 			if ( document.querySelector ( ".slider" ).classList.contains ( "paused" ) )
 			{
-				startslideshow = setInterval ( function ()
-				{
-					slideshow ()
-				}, 5000 );
+				startSlideShow ();
 				document.querySelector ( ".slider" ).classList.remove ( "paused" );
 				this.innerHTML = "&#10073;&#10073;";
 			}
 			else
 			{
-				clearInterval ( startslideshow );
+				clearInterval ( theSlideShow );
 				document.querySelector ( ".slider" ).classList.add ( "paused" );
 				this.innerHTML = "&#9658;";
 			}
 		};
 
+		// // //
+		// function that runs when NEXT button is clicked
+		// // //
 		document.getElementById ( "slider-next" ).onclick = function ()
-		{ // Next Button
-			var firstSlide = document.querySelector ( ".first-slide" ),
-				x          = 100 / slidecounter,
-				oldMargin  = firstSlide.getAttribute ( 'data-left' );
+		{
+			var x     = 100 / slideCounter;
+			oldMargin = firstSlide.getAttribute ( 'data-left' );
 
-			if ( slidecounter > counter )
+			if ( slideCounter > counter )
 			{
 				newMargin = Number ( oldMargin ) + Number ( x );
 				counter ++;
@@ -90,16 +112,18 @@ function slider ()
 
 			document.querySelector ( '.first-slide' ).style.marginLeft = '-' + newMargin + '%';
 			firstSlide.setAttribute ( 'data-left', newMargin );
-			clearInterval ( startslideshow );
+			clearInterval ( theSlideShow );
 			document.querySelector ( '.slider' ).classList.add ( "paused" );
 			document.getElementById ( 'pauseSlider' ).innerHTML = "&#9658;";
 		};
 
+		// // //
+		// function that runs when PREVIOUS button is clicked
+		// // //
 		document.getElementById ( "slider-prev" ).onclick = function ()
-		{  // Previous Button
-			var firstSlide = document.querySelector ( ".first-slide" ),
-				x          = 100 / slidecounter,
-				oldMargin  = firstSlide.getAttribute ( 'data-left' );
+		{
+			var x     = 100 / slideCounter;
+			oldMargin = firstSlide.getAttribute ( 'data-left' );
 
 			if ( counter > 1 )
 			{
@@ -108,17 +132,20 @@ function slider ()
 			}
 			else
 			{
-				newMargin = (100 / slidecounter) * (slidecounter - 1);
-				counter   = slidecounter;
+				newMargin = (100 / slideCounter) * (slideCounter - 1);
+				counter   = slideCounter;
 			}
 
 			document.querySelector ( '.first-slide' ).style.marginLeft = '-' + newMargin + '%';
 			firstSlide.setAttribute ( 'data-left', newMargin );
 
-			clearInterval ( startslideshow );
+			clearInterval ( theSlideShow );
 			document.querySelector ( '.slider' ).classList.add ( "paused" );
 			document.getElementById ( 'pauseSlider' ).innerHTML = "&#9658;";
-		}
+		};
+
+		// runs the timeOut function that makes the next slide rotate every X seconds
+		startSlideShow ();
 	}
 
 }
